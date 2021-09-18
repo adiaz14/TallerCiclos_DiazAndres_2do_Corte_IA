28c0fdf9edb69037ddf38bcd6978b0eacdc7af54
Andres Diaz <adiaz14@live.com>
2021-09-17 21:03:38 -0500

    Tercer Commit

--- TallerCiclos_DiazAndres_2do_Corte_IA.py
+++ TallerCiclos_DiazAndres_2do_Corte_IA.py
@@ -81,3 +81,84 @@ print('\n----- DEPTO DE SEGURIDAD DE TRANSITO DE BARRANQUILLA (RESUMEN '
       'DE MOVILDIAD) -----\n')
 n_autos = int(input('Ingrese cantidad de autos que ingresan a Barranquilla: '))
 conteo_calcomania(n_autos)
+
+# 2. Un Zoólogo pretende determinar el porcentaje de animales que hay en las
+# siguiente categorias de edades: 0 a 1 año, de mas de 1 año y menos de 3 y
+# de 3 o mas años. El zoológico todavía no está seguro del animal que va
+# estudiar. Si se decide por elefantes solo tomará una muestra de 20 de ellos;
+# si se decide por jirafas, tomara 15 de muestras y si son chompancés tomará
+# 40.
+
+# Supuestos: Para el número random se ha tenido en cuenta una edad entre 1 y 56
+# debido a:
+# Edad máx media de elefantes: 56 años
+# Edad máx media de jirafas: 25 años
+# Edad máx media de chimpancés: 40 años
+
+# Generar cálculo de % de edad de animales
+
+
+def porcentaje_edades_animales(animal=''):
+    animal = animal.lower()
+    if(animal != ''):
+        acum_rango_uno = 0
+        acum_rango_dos = 0
+        acum_rango_tres = 0
+        participacion_uno = 0
+        participacion_dos = 0
+        participacion_tres = 0
+        contador_uno = 0
+        contador_dos = 0
+        contador_tres = 0
+        contador = 0
+        suma_edades = 0
+        edades = []
+        if(animal == 'elefantes'):
+            no_muestras = 20
+        elif(animal == 'jirafas'):
+            no_muestras = 15
+        elif(animal == 'chimpancés' or animal == 'chimpances'):
+            no_muestras = 40
+        else:
+            no_muestras = 0
+        if(no_muestras > 0):
+            for valor in range(no_muestras):
+                edad_animal = random.uniform(0.1, 56)
+                if(edad_animal > 0 and edad_animal <= 1):
+                    acum_rango_uno += edad_animal
+                    contador_uno += 1
+                elif(edad_animal > 1 and edad_animal <= 3):
+                    acum_rango_dos += edad_animal
+                    contador_dos += 1
+                else:
+                    acum_rango_tres += edad_animal
+                    contador_tres += 1
+                edades.append(edad_animal)
+                suma_edades += edad_animal
+            participacion_uno = round((acum_rango_uno / suma_edades) * 100, 2)
+            participacion_dos = round((acum_rango_dos / suma_edades) * 100, 2)
+            participacion_tres = round(
+                (acum_rango_tres / suma_edades) * 100, 2)
+            print('\n---------- RESULTADOS DE LAS MUESTRAS ----------\n')
+            print(f'Animal estudiado: {animal.capitalize()}')
+            print(f'Tamaño de la muestra: {no_muestras}')
+            print(f'% edades entre los 0 - 1 años: {participacion_uno}%')
+            print(
+                f'% edades de más de 1 año y menos de 3: {participacion_dos}%')
+            print(f'% de edades mayor a 3 años: {participacion_tres}%')
+            print('Listado de muestras tomadas: ')
+            for edad in edades:
+                contador += 1
+                print(f'Muestra No {contador}: {round(edad,2)}')
+        else:
+            print('\nNo se suministró un nombre de animal válido (Elefantes,'
+                  ' Jirafas, Chimpancés).')
+    else:
+        print('\nNo se suministró un nombre de animal válido (Elefantes, '
+              'Jirafas, Chimpancés).')
+
+
+print('\n---------- TOMA DE MUESTRAS DE EDADES DE ANIMALES ----------\n')
+animal = input('Ingrese nombre de animal a estudiar (Elefantes, '
+               'Jirafas, Chimpancés): ')
+porcentaje_edades_animales(animal)
